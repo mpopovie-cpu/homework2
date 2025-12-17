3e5a23c4ad10936e00ac36a046961583bd45a0b2
Volodymir Popov <m.popov.ie@gmail.com>
2025-12-17 17:17:58 +0000

    task 1

--- src/main/java/homework2/HomeWork2.java
+++ src/main/java/homework2/HomeWork2.java
@@ -3,6 +3,14 @@ package homework2;
 public class HomeWork2 {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+    class Homework {
+
+        // Задание 1: печать строки указанное количество раз
+            public static void printStringTimes(int times, String text) {
+                for (int i = 0; i < times; i++) {
+                    System.out.println(text);
+                }
+            }
+        }
     }
 }
