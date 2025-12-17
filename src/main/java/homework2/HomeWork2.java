cd6c96e693a17cf6f99ef03c49fde1f18dc8748a
Volodymir Popov <m.popov.ie@gmail.com>
2025-12-17 17:19:37 +0000

    task 2

--- src/main/java/homework2/HomeWork2.java
+++ src/main/java/homework2/HomeWork2.java
@@ -1,16 +1,23 @@
 package homework2;
 
 public class HomeWork2 {
-    public static void main(String[] args) {
 
-    class Homework {
 
-        // Задание 1: печать строки указанное количество раз
-            public static void printStringTimes(int times, String text) {
-                for (int i = 0; i < times; i++) {
-                    System.out.println(text);
-                }
+        // Задание 1
+        public static void printStringTimes(int times, String text) {
+            for (int i = 0; i < times; i++) {
+                System.out.println(text);
             }
         }
+
+        // Задание 2: сумма элементов массива больше 5
+        public static void sumGreaterThanFive(int[] array) {
+            int sum = 0;
+            for (int num : array) {
+                if (num > 5) {
+                    sum += num;
+                }
+            }
+            System.out.println(sum);
+        }
     }
-}
