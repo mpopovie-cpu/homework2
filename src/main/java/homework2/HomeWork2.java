d37a71b6cf9317946be33b3618c7a13cb5d22745
Volodymir Popov <m.popov.ie@gmail.com>
2025-12-17 17:27:41 +0000

    task 3

--- src/main/java/homework2/HomeWork2.java
+++ src/main/java/homework2/HomeWork2.java
@@ -2,22 +2,35 @@ package homework2;
 
 public class HomeWork2 {
 
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        fillArray(7, arr);
+    }
 
-        // Задание 1
-        public static void printStringTimes(int times, String text) {
-            for (int i = 0; i < times; i++) {
-                System.out.println(text);
-            }
-        }
-
-        // Задание 2: сумма элементов массива больше 5
-        public static void sumGreaterThanFive(int[] array) {
-            int sum = 0;
-            for (int num : array) {
-                if (num > 5) {
-                    sum += num;
-                }
-            }
-            System.out.println(sum);
+    // Задание 1
+    public static void printStringTimes(int times, String text) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(text);
         }
     }
+
+    // Задание 2
+    public static void sumGreaterThanFive(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            if (num > 5) {
+                sum += num;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    //  Задание 3
+    public static void fillArray(int value, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+
+
+}
